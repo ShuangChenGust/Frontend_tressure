3be740760b35cd2388e3ad65b4b9b01816b39883
shuangchen <guschenwpi@gmail.com>
2020-09-01 22:24:26 -0400

    small react button eg

--- HTTP Method/fetchAPI2.js
+++ HTTP Method/fetchAPI2.js
@@ -1,33 +1,23 @@
 
 
-// var getPosts = function() {
-//   return fetch('http://jsonplaceholder.typicode.com/posts')
-//     .then(res => res.json())
-//     .then(posts => console.log(posts))
-// } ();
+var getPosts = function() {
+  return fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(posts => console.log(posts))
+} ();
 
-// console.log(getPosts);
+console.log(getPosts);
 
-// Example POST method implementation:
-async function postData(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
-  
-  postData('http://jsonplaceholder.typicode.com/posts', { answer: 42 })
-    .then(data => {
-      console.log(data); // JSON data parsed by `data.json()` call
-    });
+//use fetch to get data.
+let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+let response = await fetch(url);
+
+let commits = await response.json(); // read response body and parse as JSON
+
+alert(commits[0].author.login);
+
+
+//pure promise function:
+fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+  .then(response => response.json())
+  .then(commits => alert(commits[0].author.login));
